a8cfa99c7b57ea4208916987a64001d6a3bc72c4
Jugayganavegasbett <facundourribarri@gmail.com>
2025-09-20 12:23:07 -0300

    Update templates.js

--- assets/templates.js
+++ assets/templates.js
@@ -1,5 +1,6 @@
-// Catálogos editables
-window.CATALOG = {
-  roles: ["Victima","Imputado","Denunciante","Testigo","PP","Aprehendido","Detenido","Menor","NN","Damnificado institucional"]
-};
+// Catálogos editables (robustos: si otro script lee antes, evitamos undefined)
+window.CATALOG = Object.assign(
+  { roles: ["Victima","Imputado","Denunciante","Testigo","PP","Aprehendido","Detenido","Menor","NN","Damnificado institucional"] },
+  window.CATALOG || {}
+);
 
