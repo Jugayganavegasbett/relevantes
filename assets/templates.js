fca24296f3c01df3d95d0f5e7f2b86e566507a71
Jugayganavegasbett <facundourribarri@gmail.com>
2025-09-20 12:45:08 -0300

    Update templates.js

--- assets/templates.js
+++ assets/templates.js
@@ -1,9 +1,11 @@
 // assets/templates.js
 (function(){
   const defaults = {
-    roles: ["Victima","Imputado","Denunciante","Testigo","PP","Aprehendido","Detenido","Menor","NN","Damnificado institucional"]
+    roles: [
+      "Victima","Imputado","Denunciante","Testigo","PP",
+      "Aprehendido","Detenido","Menor","NN","Damnificado institucional"
+    ]
   };
-  // si ya existe CATALOG lo conservamos y completamos
   window.CATALOG = Object.assign({}, defaults, window.CATALOG || {});
 })();
 
